4ca716c318206854bd8bf07171733066e8debed2
Francisco Fierro <fafierrojr@gmail.com>
2018-07-10 14:21:20 -0700

    changed schools id to acronym based key

--- Models/School.cs
+++ Models/School.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace personalsiteapi.Models{
     public class School{
-        public long Id {get; set;}
+        [Key]
+        public string Key {get; set;}
+        [Required]
          public string Name {get; set;}
+        [Required]
         public string ImgUrl {get; set;}
+        [Required]
         public double Gpa{get; set;}
+        [Required]
         public string StartYear {get; set;}
+        [Required]
         public string EndYear {get; set;}
-
+        [Required]
         public string[] Honors {get; set;}
 
         // public virtual ICollection<Courses> NotableCourses;
